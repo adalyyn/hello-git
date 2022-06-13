4d82aca0eb1f38e068baab8abdaaace4495a03e2
younjeong <younj6724@gmail.com>
2022-06-13 17:38:41 +0900

    Dog 기능 완성!

--- src/com/kh/git/HelloGit.java
+++ src/com/kh/git/HelloGit.java
@@ -1,8 +1,14 @@
 package com.kh.git;
 
+import com.kh.git.animal.Dog;
+
 public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!!!");
+		
+		//Dog 기능추가
+		Dog dog = new Dog();
+		dog.bark();
 	}
 }
